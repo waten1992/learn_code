1194d99765d0533d1553797f4127b3b22ccac5d4
waten <waten1992@gmail.com>
2016-08-27 23:59:53 +0800

    update  inner interface

--- spilt_dce_symbol/split_dce.c
+++ spilt_dce_symbol/split_dce.c
@@ -1,16 +1,55 @@
 #include "split_dce.h"
 
+static binary_header_t  s_g_binary_common_head_info;
+
 void
 set_log_file_len(log_info_t *handle, unsigned int buf_size)
 {
 	handle->file_len = buf_size;
 }
 
+void 
+set_binary_common_head(binary_header_t *original_info)
+{
+	s_g_binary_common_head_info = *original_info;
+}
+
+void
+get_binary_common_head(char *addr)
+{
+	binary_header_t *set_head = (binary_header_t *)(addr);
+	
+	set_head->type = s_g_binary_common_head_info->type;
+	set_head->type_len = s_g_binary_common_head_info->type_len;
+}
+
+
 int 
-split_create(quote_struct_t *quote_file)
+split_create(quote_struct_t *quote_file, char *symbol, char *quote)
+{
+	char *quote_type_name[] = { NULL, "_best_and_deep", "_match_price", "_order_statistic", "_realtime_price", "_ten_entrust"};
+
+	/* malloc some memory store split contract info, default 10M for every contract*/
+	quote_file->mmap_addr = calloc(1, MAX_SYM_SIZE);
+	if (quote_file->mmap_addr == NULL) {
+		printf("[ERROR]-[MEMORY]-[FAIL] FILE:%s ,LINE:%d, FUNC:%s \n",
+				__FILE__,
+				__LINE__,
+				__FUNCTION__);
+		return -1;
+	}
+
+	strncpy(quote_file->symbol, symbol, sizeof(quote_file->symbol));
+	get_binary_common_head(quote_file->mmap_addr);
+	quote_file->cur_write_offset += BINARY_HEAD_CNT;
+}
+
+int 
+split_append(quote_struct_t  *quote_file)
 {
 
 }
+
 
 int 
 parser_quote_header(split_node_t *node , char *quote_path, binary_header_t *binary_info)
@@ -81,14 +120,12 @@ unsigned int
 calculate_hash_key(char *symbol)
 {	
 	unsigned int hash_key;
+
 	/*dce symbol like: a1609, cs1609, according to current contract designed max len 7bytes*/
-	if (1 != symbol[TEST_SYM_IDX]) 
-	{
+	if (1 != symbol[TEST_SYM_IDX]) {
 		/*it means just like cs1609 */
 		hash_key = atoi(symbol)*100 + symbol[0];
-	}
-	else 
-	{
+	} else {
 		hash_key = atoi(symbol)*10000 + symbol[0] + symbol[1];
 	}
 
@@ -102,15 +139,19 @@ get_hash_table_idx(unsigned int hash_key)
 }
 
 static unsigned int 
-get_best_and_deep(quote_struct_t *quote)
+get_best_and_deep(char  *quote, char *symbol)
 {
+	unsigned int hash_key = 0;
 	char *tmp = ((struct dce_my_best_deep_quote *)(quote->mmap_addr + quote->cur_write_offset))->Contract;
-	quote->hash_key = calculate_hash_key(tmp);
+	
+	strncpy(symbol, tmp, MAX_SYMBOL_LEN);
 
-	return get_hash_table_idx(quote->hash_key);
+	hash_key = calculate_hash_key(symbol);
+
+	return get_hash_table_idx(hash_key);
 }
 
-typedef unsigned int(*process_quote_symbol_t) (quote_struct_t *quote);
+typedef unsigned int(*process_quote_symbol_t) (char  *quote, char *symbol);
 
 static process_quote_symbol_t calculate_symbol_func_ar[] = 
 {
@@ -136,10 +177,10 @@ split_init(char *quote_path, split_node_t *node)
 	binary_header_t 	binary_head_info;
 	quote_struct_t  	*orginal_file;
 	char *current_addr;
+	char symbol[MAX_SYMBOL_LEN];
 	memset(node, 0, sizeof(split_node_t));
 	ret = parser_quote_header(node , quote_path, &binary_head_info);
-	if (ret != 0) 
-	{
+	if (ret != 0){
 		printf("[ERROR]: FILE:%s ,LINE:%d, FUNC:%s \n",
 				__FILE__,
 				__LINE__,
@@ -149,16 +190,21 @@ split_init(char *quote_path, split_node_t *node)
 	/* orginal node store orginal binary quote file infomation*/
 	orginal_file = &node->orginal_node;
 
-	//check_quote_type(binary_head_info);
 
-	for (idx = 0; i < binary_head_info->sym_cnt; idx++)
-	{
-			current_addr = get_current_quote_addr(orginal_file);
-			hash_idx = calculate_symbol_func_ar[binary_info->type](current_addr);
-			if (node->node_ar[hash_idx].mmap_addr == NULL)
-			{
-				split_create(node->node_ar[hash_idx]);
-			}
+	set_binary_common_head(binary_head_info);
+
+	/* scanning original binary file */
+	for (idx = 0; i < binary_head_info->sym_cnt; idx++) {
+
+		current_addr = get_current_quote_addr(orginal_file);
+		
+		hash_idx = calculate_symbol_func_ar[binary_info->type](current_addr, symbol);
+		if (node->node_ar[hash_idx].mmap_addr == NULL) {
+			split_create(node->node_ar[hash_idx], symbol, current_addr);
+		} else {
+			split_append(node->node_ar[hash_idx], symbol, current_addr);
+		}
+
 	}
 
 
--- spilt_dce_symbol/split_dce.h
+++ spilt_dce_symbol/split_dce.h
@@ -1,29 +1,36 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h> 
 #include <errno.h>
 #include <string.h>
 #include <sys/mman.h>
 
-#define MAX_SYMBOL_LEN  (16)
+#define MAX_SYMBOL_LEN  (8)
 #define MAX_SYM_IDX  	(256)
-#define MAX_SYM_SIZE	(1<<24)
+#define MAX_SYM_SIZE	(10<<20)
 #define TIMESTAMP_LEN	(8)
-#define TYPE_LEN 		(2)
+#define TYPE_LEN 		(6)
 #define TEST_SYM_IDX	(1)
 #define PRIME_NUM  		(253) 
+#define BINARY_HEAD_CNT (8)
 
 /*for speed use hash instead of travel compare */
 
-typedef struct binary_header{
+typedef struct binary_header
+{
+	unsigned int 	sym_cnt;
 	unsigned short 	type;
 	unsigned short  type_len;
-	unsigned int 	sym_cnt;
 }binary_header_t;
 
 
-typedef struct quote_struct {
+typedef struct quote_struct 
+{
 	unsigned int		hash_key;/*calcluate by symbol*/
+	char 				symbol[MAX_SYMBOL_LEN];
 	char 				*mmap_addr;
 	unsigned int 		cur_write_offset;
 	unsigned int 		file_len;
