84c1465783de2d822b0fe6ddea4377af0a5aedbd
waten <waten1992@gmail.com>
2016-08-28 22:38:25 +0800

    delete name spell wrong
    finish code , waiting for test

--- split_dce_symbol/split_dce.c
+++ split_dce_symbol/split_dce.c
@@ -23,12 +23,48 @@ get_binary_common_head(char *addr)
 	set_head->type_len = s_g_binary_common_head_info->type_len;
 }
 
+unsigned short 
+get_quote_type_len()
+{
+	return (s_g_binary_common_head_info.type_len);
+}
+
+
+unsigned short
+get_quote_type()
+{
+	return (s_g_binary_common_head_info.type);
+}
+
+void 
+update_binary_offset(quote_struct_t *quote_file)
+{
+	quote->cur_write_offset += get_quote_type_len();
+}
+
+void 
+update_binary_symbol_cnt(quote_struct_t *quote_file)
+{
+	quote_file->mmap_addr = *(unsigned int *)(quote_file->mmap_addr) + 1;
+}
+
+void
+cp_quote_struct_and_update(quote_struct_t *quote_file)
+{
+	/* copy orginal file info to new file */
+	cur_addr = get_current_quote_addr(quote_file);
+	type_len = get_quote_type_len();
+	strncpy(cur_addr, quote, type_len);
+	
+	update_binary_offset(quote_file);
+	update_binary_symbol_cnt(quote_file);
+}
 
 int 
 split_create(quote_struct_t *quote_file, char *symbol, char *quote)
 {
-	char *quote_type_name[] = { NULL, "_best_and_deep", "_match_price", "_order_statistic", "_realtime_price", "_ten_entrust"};
-
+	char *cur_addr;
+	unsigned int type_len;
 	/* malloc some memory store split contract info, default 10M for every contract*/
 	quote_file->mmap_addr = calloc(1, MAX_SYM_SIZE);
 	if (quote_file->mmap_addr == NULL) {
@@ -38,16 +74,56 @@ split_create(quote_struct_t *quote_file, char *symbol, char *quote)
 				__FUNCTION__);
 		return -1;
 	}
-
+	/*set binary header offset info*/
+	quote_file->cur_write_offset += BINARY_HEAD_CNT;
+	
 	strncpy(quote_file->symbol, symbol, sizeof(quote_file->symbol));
 	get_binary_common_head(quote_file->mmap_addr);
-	quote_file->cur_write_offset += BINARY_HEAD_CNT;
+
+	cp_quote_struct_and_update(quote_file)
+	
+	return 0;
 }
 
 int 
-split_append(quote_struct_t  *quote_file)
+split_append(quote_struct_t *quote_file, char *symbol, char *quote)
 {
-
+	int ret = 0;
+	quote_struct_t *tmp_p, *new_quote_node;
+	tmp_p =  quote_file;
+	/*select the same contract */
+	for ( ;tmp_p->next != NULL ; tmp_p = tmp_p->next ) {
+		if (!strcmp(tmp_p->symbol, symbol)) {
+			ret = 1;
+			break;
+		}
+	} 
+	/* ret != 1 , it's means need create new node*/
+	if(ret != 1) {
+		new_quote_node = calloc(1, sizeof(quote_struct_t));
+		if (new_quote_node == NULL) {
+			printf("[ERROR]-[MEMORY]-[FAIL] FILE:%s ,LINE:%d, FUNC:%s \n",
+				__FILE__,
+				__LINE__,
+				__FUNCTION__);
+			return -1;
+		}
+		
+		ret = split_create(new_quote_node, symbol, quote);
+		if (ret != 0) {
+			printf("[ERROR]-[CREATE]-[FAIL] FILE:%s ,LINE:%d, FUNC:%s \n",
+				__FILE__,
+				__LINE__,
+				__FUNCTION__);
+			return -1;
+		}
+		tmp_p->next = new_quote_node;
+		cp_quote_struct_and_update(new_quote_node);
+	}else {
+		cp_quote_struct_and_update(tmp_p);		
+	} 
+	
+	return 0;
 }
 
 
@@ -142,7 +218,7 @@ static unsigned int
 get_best_and_deep(char  *quote, char *symbol)
 {
 	unsigned int hash_key = 0;
-	char *tmp = ((struct dce_my_best_deep_quote *)(quote->mmap_addr + quote->cur_write_offset))->Contract;
+	char *tmp = ((struct dce_my_best_deep_quote *)(quote + TIMESTAMP_LEN))->Contract;
 	
 	strncpy(symbol, tmp, MAX_SYMBOL_LEN);
 
@@ -166,7 +242,6 @@ static process_quote_symbol_t calculate_symbol_func_ar[] =
 char *
 get_current_quote_addr(quote_struct_t *quote)
 {
-	quote->cur_write_offset += TIMESTAMP_LEN;
 	return (quote->mmap_addr + quote->cur_write_offset);
 }
 
@@ -177,6 +252,7 @@ split_init(char *quote_path, split_node_t *node)
 	binary_header_t 	binary_head_info;
 	quote_struct_t  	*orginal_file;
 	char *current_addr;
+	
 	char symbol[MAX_SYMBOL_LEN];
 	memset(node, 0, sizeof(split_node_t));
 	ret = parser_quote_header(node , quote_path, &binary_head_info);
@@ -185,6 +261,8 @@ split_init(char *quote_path, split_node_t *node)
 				__FILE__,
 				__LINE__,
 				__FUNCTION__);
+				
+		return -1;
 	}
 
 	/* orginal node store orginal binary quote file infomation*/
@@ -205,7 +283,93 @@ split_init(char *quote_path, split_node_t *node)
 			split_append(node->node_ar[hash_idx], symbol, current_addr);
 		}
 
+
 	}
 
+	return 0;
+}
 
+int 
+orginal_binary_destory(quote_struct_t *quote)
+{
+	int ret = 0;
+	if (quote->mmmap_addr) {
+		msync(quote->mmap_addr, quote->file_len, MS_ASYNC);
+		
+		ret = munmap(quote->mmap_addr, quote->file_len);
+		if (-1 == ret) {
+			printf("[ERROR]: FILE:%s ,LINE:%d, FUNC:%s got error\n",
+					__FILE__,
+					__LINE__,
+					__FUNCTION__);
+			close(quote->fd);
+			return -1;
+		}
+
+		quote->mmap_addr = NULL;
+		close(quote->fd);
+	}
+	return 0;
+}
+
+int 
+flush_data_to_disk(quote_struct_t *quote)
+{
+	char *quote_type_name[] = { NULL, "_best_and_deep", "_match_price", "_order_statistic", "_realtime_price", "_ten_entrust"};
+	char quote_name_path[FLUSH_NAME_LEN];
+	unsigned short type = get_quote_type();
+	FILE *fd;
+	quote_struct_t *tmp_p = quote;
+
+	/*Traversal hash index all node */
+	for ( ;tmp_p->next != NULL ; tmp_p = tmp_p->next ) {
+	
+		snprintf(quote_name_path, FLUSH_NAME_LEN,"%s%s",tmp_p->symbol,quote_type_name[type]);
+
+		fd = fopen(quote_name_path,"w+");
+		if (fd == NULL) {
+			printf("[ERROR]: FILE:%s ,LINE:%d, FUNC:%s got error\n",
+					__FILE__,
+					__LINE__,
+					__FUNCTION__);
+			return -1;
+		}
+
+		fwrite(tmp_p->mmap_addr, tmp_p->cur_write_offset, 1, fd);
+		fflush(fd);
+		fclose(fd);
+		fd = NULL;	
+	}
+
+	return 0;
+}
+
+int 
+split_destory(split_node_t *node)
+{
+	int ret = 0;
+	/*the first destory original binary file*/
+	quote_struct_t  	*orginal_file, *node_ar;
+	orginal_file = &node->orginal_node;
+	node_ar = &node->node_ar;
+
+	ret = orginal_binary_destory(orginal_file);
+	if (ret != 0) {
+		printf("[ERROR]: FILE:%s ,LINE:%d, FUNC:%s got error\n",
+					__FILE__,
+					__LINE__,
+					__FUNCTION__);
+	}
+
+	for (int idx = 0; idx < MAX_SYM_IDX; idx++) {
+		if(node_ar[idx].mmap_addr != NULL) {
+			ret = flush_data_to_disk(node_ar[idx]);
+			if (ret != 0) {
+				printf("[ERROR]: FILE:%s ,LINE:%d, FUNC:%s got error\n",
+					__FILE__,
+					__LINE__,
+					__FUNCTION__);
+			}
+		}
+	}
 }
