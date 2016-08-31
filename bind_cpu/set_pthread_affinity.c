3936d03b35212ddaef6629e062d3202737c3a37c
waten <waten1992@gmail.com>
2016-08-31 16:03:05 +0800

    bind cpu example

---
+++ bind_cpu/set_pthread_affinity.c
@@ -0,0 +1,74 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <sched.h>
+#include <pthread.h>
+
+int
+pthread_bind_cpu(unsigned int cpu_no)
+{
+	int ret = 0;
+	cpu_set_t cpuset;
+	pthread_t ptd_id;
+	ptd_id = pthread_self();
+
+	CPU_ZERO(&cpuset);
+
+	if(CPU_ISSET(cpu_no, &cpuset)) {
+		printf("cpu_no:%d was set affinity !\n", cpu_no);
+		return -1;	
+	} 
+
+	CPU_SET(cpu_no, &cpuset);
+	ret = pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+	if (ret != 0) {
+		printf("[ERROR-SET-AFFINITY] FILE:%s ,LINE:%d, FUNC:%s \n",
+			__FILE__,
+			__LINE__,
+			__FUNCTION__);
+
+		return -1;
+	}
+
+	return 0;
+}
+
+/* src like: "place,a1609,0,0,4,3756", return split count number*/
+int
+parser_splite_by_delim(char *src, char *ar[], char delim)
+{
+    int idx = 0;
+    if(*src != delim) {
+        ar[idx++] =  src;
+    }
+
+    while(*src) {
+        if (*src == delim) {
+            *src = '\0';
+            ar[idx++] = src++;
+
+        }
+
+        src++;
+    }
+    return idx;
+}
+
+
+/*use ps -eo pid,args,psr | grep test .  look up bind cpu */
+
+int
+main()
+{
+	int ret = 0;
+	ret = pthread_bind_cpu(1);
+	if(ret != 0) {
+		printf("[ERROR-SET-AFFINITY]: FILE:%s ,LINE:%d, FUNC:%s \n",
+				__FILE__,
+				__LINE__,
+				__FUNCTION__);
+		return -1;
+	}
+
+
+	return 0;
+}
